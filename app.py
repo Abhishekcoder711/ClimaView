eb059fc3ec26a807b2fa1b9ad912e2086940f124
Abhishekcoder711 <abhisehkind711@gmail.com>
2026-04-10 01:18:59 +0530

    Add Microsoft Clarity Code

--- app.py
+++ app.py
@@ -70,6 +70,14 @@ crossorigin="anonymous"></script>
 
 <script charset="UTF-8" src="//web.webpushs.com/js/push/22a463ae4152985a4fa99e06831dc5ee_1.js" async></script>
 
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w953vp4znl");
+</script>
+
 </head>
 
 
